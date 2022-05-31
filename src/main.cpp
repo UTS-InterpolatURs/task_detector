634d2e020dff62c665311274c70dc460e2774b0f
Jon McLean <61402742+Jon-C-McLean@users.noreply.github.com>
2022-05-31 13:28:15 +1000

    Added some comments to main

--- src/main.cpp
+++ src/main.cpp
@@ -41,36 +41,37 @@ std::vector<Spatial::Coordinate> points;
 ros::Publisher pointsPub;
 
 void imageCallback(const sensor_msgs::ImageConstPtr depthMessage, const sensor_msgs::ImageConstPtr colorMessage, const sensor_msgs::CameraInfoConstPtr info) {
-
+    // Gets color and depth images as cv::Mat image type
     cv::Mat colorImage = converter.convertMessageToCVImage(colorMessage, colorMessage->encoding);
     cv::Mat depthImage = converter.convertMessageToCVImage(depthMessage, depthMessage->encoding);
 
+    // Gets all recognised features on taskboard
     Classification::Taskboard board = finder.findFeatures(colorImage, depthImage, info);
 
+    // Gets keyhole feature
     Classification::Feature feature = board.keyHole;
 
+    // Draws rectangle around the recognised features and draws circle around the center point of the feature
     cv::rectangle(colorImage, feature.boundingRect, cv::Scalar(255,255,255));
     cv::Point centerPoint = feature.centerPoint.getImageCoordinates();
     cv::circle(colorImage, cv::Point(centerPoint.x, centerPoint.y), 10, cv::Scalar(255,0,255));
 
+    // Gets centerpoint and adds to point array for filtering
     Spatial::Coordinate coord = feature.centerPoint;
     points.push_back(coord);
 
     geometry_msgs::Point32 globalPoint = coord.getGlobalCoordinates();
-
     ROS_INFO_STREAM("GX: " << globalPoint.x << " GY: " << globalPoint.y << " GZ: " << globalPoint.z);
 
+    // Checks to see if the point array has reached the required number of points to publish
     if(points.size() >= POINT_COUNT_GOAL - 1) {
         Utils::CoordinateSmoother smoother;
-        // std::vector<geometry_msgs::Point32> featurePoints;
-        // featurePoints.push_back(smoother.getSmoothedGlobalCoordinate(points));
-        // geometry_msgs::Point32 smoothedPoints[1] = {smoother.getSmoothedGlobalCoordinate(points)};
-        pointsPub.publish(smoother.getSmoothedGlobalCoordinate(points));
+        pointsPub.publish(smoother.getSmoothedGlobalCoordinate(points)); // Publishes global coordinates to topic
         points.clear();
     }
 
-    sensor_msgs::Image output = converter.convertCVImageToMessage(colorImage, colorMessage->encoding);
-    imagePub.publish(output);
+    sensor_msgs::Image output = converter.convertCVImageToMessage(colorImage, colorMessage->encoding); // Converts processed image to message
+    imagePub.publish(output); // Publishes processed image message
 }
 
 bool getFeature(task_detector::GetFeature::Request &request, task_detector::GetFeature::Response &response) {
@@ -95,25 +96,34 @@ int main(int argc, char **argv) {
 
     pointsPub = n.advertise<geometry_msgs::Point32>("/task_detect/detected_points", 1);
     
+    // Creates image output stream topic
     image_transport::ImageTransport transport(n);
-    // image_transport::Subscriber imageSub = transport.subscribe("/camera/color/image_raw", 1, &imageCallback);
     imagePub = transport.advertise("task_detect/output_video", 10);
 
+    // Initializes task finder
     finder = Classification::TaskFinder(n);
 
+    // Creates synchronised subscribers
     message_filters::Subscriber<sensor_msgs::Image> depthSub(n, "/camera/aligned_depth_to_color/image_raw", 1);
     message_filters::Subscriber<sensor_msgs::Image> imageSub(n, "/camera/color/image_raw", 1);
     message_filters::Subscriber<sensor_msgs::CameraInfo> infoSub(n, "camera/color/camera_info", 1);
 
+    // Creates sync policy type definition (for readability)
     typedef message_filters::sync_policies::ExactTime<sensor_msgs::Image, sensor_msgs::Image, sensor_msgs::CameraInfo> ImageSyncPolicy;
+
+    // Creates synchronizer
     message_filters::Synchronizer<ImageSyncPolicy> sync(ImageSyncPolicy(10), depthSub, imageSub, infoSub);
+
+    // Register synchronous callback with ROS
     sync.registerCallback(boost::bind(&imageCallback, _1, _2, _3));
 
+    // Defines Service for gettting all recognised features
     ros::ServiceServer service = n.advertiseService("get_features", getFeature);
 
     // ros::spin();8888
 
-    ros::AsyncSpinner asyncSpinner(8);
+    // Creates async ros spinner so image processing and service handling can occur simulatenously
+    ros::AsyncSpinner asyncSpinner(8); // Note: This does not have to be 8 threads and can instead be any number above 3
     asyncSpinner.start();
 
     ros::waitForShutdown();
